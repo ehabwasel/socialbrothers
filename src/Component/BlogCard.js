cefa7fa0014d148e85ae6ad75a8b64fd7f2dcd94
ehabwasel <ehab.wasel@yahoo.com>
2021-10-20 12:33:48 +0200

    card img css

--- src/Component/BlogCard.js
+++ src/Component/BlogCard.js
@@ -10,37 +10,36 @@ const Blog = ({ post }) => {
         maxWidth: "230px",
         margin: "15px 5px",
         padding: "2px",
-        overflow: "hidden",
         border: "none",
         boxShadow: " rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
       }}
     >
       <Card.Header
-        className="blogImage bg-dark"
+        className="blogImageCard bg-dark"
         style={{
           backgroundImage: `url(${img_url})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          position: "center",
-          height: "100px",
         }}
       >
-        <div className="blogInfo d-flex justify-content-between align-items-end text-white">
+        <div
+          className="blogInfo d-flex justify-content-between align-items-end text-white
+        "
+        >
           <span>{created_at.substring(0, 10)}</span>
           <span>{category ? category.name : "Tech"}</span>
         </div>
       </Card.Header>
-      <Card.Body>
+      <Card.Body
+        style={{
+          fontSize: "11px",
+          overflow: "auto",
+          height: "60%",
+          textAlign: "center",
+        }}
+      >
         <Card.Title>
           <strong>{title}</strong>
         </Card.Title>
-        <Card.Text
-          as="div"
-          className="py-3 "
-          style={{
-            fontSize: "11px",
-          }}
-        >
+        <Card.Text as="div" className="py-3 ">
           {content}
         </Card.Text>
       </Card.Body>
